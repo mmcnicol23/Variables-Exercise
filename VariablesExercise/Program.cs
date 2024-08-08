c4d929829702926aa4a440b47352a01688a2e2d9
Mariah McNicol <mariahmcnicol@gmail.com>
2024-08-08 12:04:00 -0400

    created a story using variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Mariah";
+            string secondName = "Shane";
+            int number = 2;
+            double price = 10.99;
+            bool isSmart = true;
+            char letter = 'A';
+            decimal age = 10.5m;
+
+//Here is the story using the variables above:
+            Console.WriteLine($"{name} and {secondName} have {number} dogs at home.");
+            Console.WriteLine($"Last night, they paid {price} for a pizza for dinner.");
+            Console.WriteLine($"It's {isSmart} that Shane is very funny and handsome.");
+            Console.WriteLine($"Mariah is going to get an {letter} on her assignments at TrueCoders.");
+            Console.WriteLine(
+                $"{name} and {secondName} took their dog on a trip for her half birthday. She is now {age} years old.");
         }
     }
 }
